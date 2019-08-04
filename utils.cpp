38d8456ee123b90dc33cc94df203ef9e6318a40a
alastairmunro <alastair.munro@qrious.co.nz>
2019-08-05 11:03:45 +1200

    Moved function to utility function file

---
+++ utils.cpp
@@ -0,0 +1,12 @@
+#include "utils.h"
+#include <iostream>
+
+void print_byte_array_as_decimal(const unsigned char* str) {
+        const int length = strlen((char*)str);
+        
+        std::cout << "[  ";
+        for (int i = 0; i < length; i++) {
+            std::cout << (int)str[i] << "  ";
+        }
+        std::cout << "]" << std::endl;
+}
---
+++ utils.h
@@ -0,0 +1,7 @@
+#ifndef UTILS_H
+#define UTILS_H
+// This file contains various utilities for aiding in development.
+
+void print_byte_array_as_decimal(const unsigned char* str);
+
+#endif
