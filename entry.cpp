851a39c3b0e4e670ea5cadc2cfd2b5ad21a4bc83
alastairmunro <alastair.munro@qrious.co.nz>
2019-08-05 11:03:22 +1200

    Added skeleton files for entry class

---
+++ entry.cpp
@@ -0,0 +1,7 @@
+#include "entry.h"
+
+class Entry {
+    std::string title;
+    std::string username;
+    std::string password;
+};
---
+++ entry.h
@@ -0,0 +1,8 @@
+#ifndef ENTRY_H
+#define ENTRY_H
+
+#include <string>
+
+class Entry;
+
+#endif
