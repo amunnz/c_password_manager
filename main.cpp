1daa7bd8413d414175583907871f4e67b10e2e61
alastairmunro <alastair.munro@qrious.co.nz>
2019-08-05 11:47:44 +1200

    Included new header

--- main.cpp
+++ main.cpp
@@ -4,14 +4,13 @@ int main(int argc, char** argv) {
     
     std::cout << "++++++++++++++++++++++++++++++++++++++++++++++++" << std::endl;
 
-
     byte* message = (byte*)"Title,Username,Password\nFacebook,email@address.com,myfbpassword";
 
     // Assume there is a file to open with a iv and encrypted data
     //CD cryptographic_data = CD();
 
     // This should be done as late as possible.
-    CD cryptographic_data = CD(message);
+    CryptoG cryptographic_data = CryptoG(message);
     cryptographic_data.encrypt_and_write_to_file();      
     
     return 0;
