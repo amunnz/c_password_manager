d34607d0ca6233bb4873e3e10ebaedd98838eae4
= <=>
2019-08-05 17:06:05 +1200

    Updated print function definition to take message length

--- utils.cpp
+++ utils.cpp
@@ -1,11 +1,10 @@
 #include "utils.h"
-#include <iostream>
 
-void print_byte_array_as_decimal(const unsigned char* str) {
-        const int length = strlen((char*)str);
+void print_byte_array_as_decimal(const unsigned char* str, const size_t length) {
+        
         std::cout << "[  ";
         for (int i = 0; i < length; i++) {
-            std::cout << (int)str[i] << "  ";
+            std::cout << std::dec << (int)str[i] << "  ";
         }
         std::cout << "]" << std::endl;
 }
