64cbecdf235f38f2351ce1c35ce844a3a5fa1e45
amunnz <a.n_munro@outlook.com>
2019-08-05 17:00:59 +1200

    Moved standard library includes to header

--- utils.h
+++ utils.h
@@ -2,6 +2,9 @@
 #define UTILS_H
 // This file contains various utilities for aiding in development.
 
-void print_byte_array_as_decimal(const unsigned char* str);
+#include <iostream>
+#include <cstring>
+
+void print_byte_array_as_decimal(const unsigned char* str, const size_t length);
 
 #endif
