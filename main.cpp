4c4c7ca5166e3a9db61eeb72d892d212b9ff0492
= <=>
2019-08-05 21:25:10 +1200

    Improve comments

--- main.cpp
+++ main.cpp
@@ -3,20 +3,18 @@
 
 int main(int argc, char** argv) {
 
-    const std::string message = "Title,Username,Password\nFacebook,email@address.com,myfbpassword";
-
-    // Initialise a pointer to our object. If no file is provided, point to 0x0, otherwise allocate
-    // a new CryptoG object on the heap and point to it.
+    // Point to null if no file argument, otherwise allocate a new CryptoG object.
     CryptoG* cryptographic_data = (argc == 1) ? nullptr : new CryptoG();
 
     // Do database stuff...
     UI::print_home_screen();
+    const std::string message = "Title,Username,Password\nFacebook,email@address.com,myfbpassword";
 
-    // If cryptographic_data hasn't been initialised away from NULLPTR, initialise it
+    // This is the code for case 6, save to file and close
     if (!cryptographic_data) {
         cryptographic_data = new CryptoG(message, message.length());
     }
-    cryptographic_data->encrypt_and_write_to_file();     
+    cryptographic_data->encrypt_and_write_to_file();
     delete cryptographic_data;
 
     return 0;
