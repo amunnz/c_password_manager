7ada20e732c2f919001e85001c2525dcd20f45d1
alastairmunro <alastair.munro@qrious.co.nz>
2019-08-05 13:45:52 +1200

    Removed whitespace

--- utils.cpp
+++ utils.cpp
@@ -3,7 +3,6 @@
 
 void print_byte_array_as_decimal(const unsigned char* str) {
         const int length = strlen((char*)str);
-        
         std::cout << "[  ";
         for (int i = 0; i < length; i++) {
             std::cout << (int)str[i] << "  ";
