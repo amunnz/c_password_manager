758b06331d88b1d1c999f6bb0c3618eb420ffbd1
amunnz <a.n_munro@outlook.com>
2019-08-10 21:42:52 +1200

    Declared enum UserChoice and function get_user_action_choice

--- ui.h
+++ ui.h
@@ -5,7 +5,11 @@
 
 class UI {
     public:
+    enum UserChoice {
+        PRINT_ALL, PRINT_SINGLE, ADD, DELETE, EDIT, SAVE_AND_EXIT, DISCARD_AND_EXIT, LAST_ITEM
+    };
     static void print_home_screen();
+    static UserChoice get_user_action_choice();
 };
 
 #endif
