6d86ef4b57bc40b03b72299c159708bb3b8f1c15
amunnz <a.n_munro@outlook.com>
2019-08-11 19:19:38 +1200

    Add space

--- ui.h
+++ ui.h
@@ -6,6 +6,7 @@
 class UI {
 private:
     static bool is_number(const std::string&);
+    
 public:
     enum UserChoice {
         PRINT_ALL, PRINT_SINGLE, ADD, DELETE, EDIT, SAVE_AND_EXIT, DISCARD_AND_EXIT, LAST_ITEM
